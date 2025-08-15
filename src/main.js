7bfd37ae5e2f896be22885c811a9ed23f57234eb
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-08-15 10:19:44 +0800

    更新依赖项至最新版本，添加 @bingwu/iip-ui-utils 和 vxe-pc-ui，修改主入口文件以引入新组件，优化 BasicView.vue 以展示更丰富的表格功能和交互，提升用户体验。

--- src/main.js
+++ src/main.js
@@ -10,11 +10,14 @@ import IipUI from '@bingwu/iip-ui-components'
 import '@bingwu/iip-ui-theme/dist/index.css'
 import VxeUITable from 'vxe-table'
 import 'vxe-table/lib/style.css'
+import VxePCUI from 'vxe-pc-ui'
+import 'vxe-pc-ui/lib/style.css'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(VxeUITable)
 app.use(ElementPlus)
+app.use(VxePCUI)
 app.use(IipUI)
 app.mount('#app')
